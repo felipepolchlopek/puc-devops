bd10ab659e8b983aaad2b7d649d4c4bb46fa2c5b
felipepolchlopek <polchlopekf@gmail.com>
2024-09-16 11:57:40 -0300

    criação do tabuleiro

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 const (
 	EMPTY    = ' '
 	PLAYER_X = 'X'
@@ -17,4 +19,22 @@ func initializeBoard() {
 		}
 	}
 	currentPlayer = PLAYER_X
+}
+
+// Exibe o tabuleiro
+func displayBoard() {
+	fmt.Println("  0   1   2")
+	for i := 0; i < 3; i++ {
+		fmt.Printf("%d", i)
+		for j := 0; j < 3; j++ {
+			fmt.Printf(" %c ", board[i][j])
+			if j < 2 {
+				fmt.Print("|")
+			}
+		}
+		fmt.Println()
+		if i < 2 {
+			fmt.Println(" ---|---|---")
+		}
+	}
 }
